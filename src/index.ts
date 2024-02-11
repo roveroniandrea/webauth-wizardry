df2c277e563e686d25cba8e56702575d6dafa08e
roveroniandrea <roveroniandrea@gmail.com>
2024-02-11 20:40:46 +0100

    init express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-type A = {}
+import express from 'express';
+const app = express();
+const port = 3000;
 
-console.log("Hello world")
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
